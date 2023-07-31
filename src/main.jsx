fefec45ff39db49535f733e03097a9353c87e4c0
StanislavPoponia <127778439+StanislavPoponia@users.noreply.github.com>
2023-07-31 11:35:14 +0300

    basename /

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <BrowserRouter basename="/goit-test-task/">
+        <BrowserRouter basename="/">
           <App />
         </BrowserRouter>
       </PersistGate>
